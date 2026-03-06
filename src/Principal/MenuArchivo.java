9e06ed8b7161e56a7fe945361f41153102913cbe
nnasryy <nasrywilliamsbali@gmail.com>
2026-03-06 10:24:54 -0600

    Mis Clases

--- src/Principal/MenuArchivo.java
+++ src/Principal/MenuArchivo.java
@@ -4,10 +4,71 @@
  */
 package Principal;
 
+import java.io.File;
+import javax.swing.JFileChooser;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JTextPane;
+
 /**
  *
  * @author nasry
  */
-public class MenuArchivo {
+public class MenuArchivo extends JMenuBar{
+    JMenu archivo;
+    JMenuItem nuevo, abrir, guardar, salir;
     
+    GestorDocx gestor;
+    JTextPane areaTexto;
+    
+     public MenuArchivo(JTextPane areaTexto) {
+
+        this.areaTexto = areaTexto;
+        gestor = new GestorDocx();
+
+        archivo = new JMenu("Archivo");
+
+        nuevo = new JMenuItem("Nuevo");
+        abrir = new JMenuItem("Abrir");
+        guardar = new JMenuItem("Guardar");
+        salir = new JMenuItem("Salir");
+
+        archivo.add(nuevo);
+        archivo.add(abrir);
+        archivo.add(guardar);
+        archivo.addSeparator();
+        archivo.add(salir);
+
+        add(archivo);
+
+        eventos();
+    }
+    
+    private void eventos(){
+    nuevo.addActionListener(e -> areaTexto.setText(""));
+    abrir.addActionListener(e -> {
+
+            JFileChooser chooser = new JFileChooser();
+
+            if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+                File archivo = chooser.getSelectedFile();
+                gestor.abrirDocx(archivo.getAbsolutePath(), areaTexto);
+            }
+
+        });
+    guardar.addActionListener(e -> {
+
+            JFileChooser chooser = new JFileChooser();
+
+            if (chooser.showSaveDialog(null) == JFileChooser.APPROVE_OPTION) {
+                File archivo = chooser.getSelectedFile();
+                gestor.guardarDocx(archivo.getAbsolutePath(), areaTexto);
+            }
+
+        });
+
+        salir.addActionListener(e -> System.exit(0));
+
+    }
 }
--- src/Principal/TablaManager.java
+++ src/Principal/TablaManager.java
@@ -3,11 +3,27 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Principal;
-
+import org.apache.poi.xwpf.usermodel.*;
 /**
  *
  * @author nasry
  */
+
 public class TablaManager {
-    
+
+    public void crearTabla(XWPFDocument doc, int filas, int columnas) {
+
+        XWPFTable tabla = doc.createTable(filas, columnas);
+
+        for (int i = 0; i < filas; i++) {
+
+            XWPFTableRow row = tabla.getRow(i);
+
+            for (int j = 0; j < columnas; j++) {
+
+                row.getCell(j).setText(" ");
+
+            }
+        }
+    }
 }
